8a8392cac84819531b2e85a16a20bc1a6ad1cef6
yewz <weiwenz@yahoo.com>
2016-02-25 19:09:30 -0500

    Update HelloWorldServlet.java
    test git plugin trigger

--- src/main/java/com/geekcap/vmturbo/HelloWorldServlet.java
+++ src/main/java/com/geekcap/vmturbo/HelloWorldServlet.java
@@ -6,7 +6,7 @@ import java.io.*;
 
 public class HelloWorldServlet extends HttpServlet 
 {
-  public void service( HttpServletRequest req, HttpServletResponse res ) throws IOException {//test webhook
+  public void service( HttpServletRequest req, HttpServletResponse res ) throws IOException {//test webhook git plugin
     PrintWriter out = res.getWriter();
     out.println( "Hello, World!" );
     out.close();
